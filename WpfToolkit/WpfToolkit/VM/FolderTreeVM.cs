d27603a3b0796dd049af771a314dcb995e5adb85
Gavin Hickford <gjhickford@gmail.com>
2014-11-09 21:39:59 +0000

    Added new WPF MVVM Project
    Added new WPF MVVM Project

---
+++ WpfToolkit/WpfToolkit/VM/FolderTreeVM.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfToolkit.VM
+{
+    public class FolderTreeVM : ViewModelBase
+    {
+        private string _title = "...........";
+
+        public string Title
+        {
+            get { return _title; }
+            set { Set(() => Title, ref _title, value); }
+        }
+
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
---
+++ WpfToolkit/WpfToolkit/VM/MainVM.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfToolkit.Main;
+
+namespace WpfToolkit.VM
+{
+    public class MainVM : ViewModelBase
+    {
+        private FolderTreeVM _childVM;
+        public MainVM()
+        {
+            ChildVM = new FolderTreeVM();
+            ChildVM.Title = "My org title";
+        }
+
+        public FolderTreeVM ChildVM 
+        {
+            get { return _childVM; }
+            set { Set(() => ChildVM, ref _childVM, value); }
+        }
+    }
+}
