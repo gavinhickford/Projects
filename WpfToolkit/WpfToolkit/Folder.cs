ef68612c3cfd80c2b49bffa2087e90555d30af42
Gavin Hickford <gjhickford@gmail.com>
2014-11-09 22:31:09 +0000

    Added databinding to treeview
    Added databinding to treeview

---
+++ WpfToolkit/WpfToolkit/Folder.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfToolkit
+{
+    public class Folder : ObservableObject
+    {
+        private string _name;
+        private ObservableCollection<Folder> _folders;
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(() => Name, ref _name, value); }
+        }
+
+        public ObservableCollection<Folder> Folders
+        {
+            get { return _folders; }
+            set { Set(() => Folders, ref _folders, value); }
+        }
+    }
+}
--- WpfToolkit/WpfToolkit/VM/FolderTreeVM.cs
+++ WpfToolkit/WpfToolkit/VM/FolderTreeVM.cs
@@ -1,6 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,63 @@ namespace WpfToolkit.VM
     public class FolderTreeVM : ViewModelBase
     {
         private string _title = "...........";
-
+        private ObservableCollection<Folder> _folders = new ObservableCollection<Folder>
+        {
+            new Folder
+            {
+                Name = "EMIS",
+                Folders = new ObservableCollection<Folder>
+                {
+                    new Folder{Name = "Qof1"},
+                    new Folder{Name = "Qof2"}
+                }
+            },
+            new Folder
+            {
+                Name = "My Org",
+                Folders = new ObservableCollection<Folder>
+                {
+                    new Folder{Name = "Org Folder1"},
+                    new Folder
+                    {
+                        Name = "Org Folder2",
+                        Folders = new ObservableCollection<Folder>
+                        {
+                            new Folder{Name = "Child1"},
+                            new Folder{Name = "Child2"}
+                        }   
+                    }
+                }
+            },
+            new Folder
+            {
+                Name = "Shared Org",
+                Folders = new ObservableCollection<Folder>
+                {
+                    new Folder{Name = "Shared Folder1"},
+                    new Folder
+                    {
+                        Name = "Shared Folder2",
+                        Folders = new ObservableCollection<Folder>
+                        {
+                            new Folder{Name = "SharedChild1"},
+                            new Folder{Name = "SharedChild2"}
+                        }   
+                    }
+                }
+            }
+        };
+   
         public string Title
         {
             get { return _title; }
             set { Set(() => Title, ref _title, value); }
         }
 
-        public void SetTitle(string title)
+        public ObservableCollection<Folder> Folders
         {
-            Title = title;
+            get { return _folders; }
+            set { Set(() => Folders, ref _folders, value); }
         }
     }
 }
