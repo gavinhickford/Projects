89cb5d43109df3eb31486f8f76eb216256b5b1e2
Gavin Hickford <gjhickford@gmail.com>
2016-12-20 15:39:10 +0000

    Added services and routing
    Added services and routing

---
+++ Angularjs/GithubViewer/GithubViewer/controllers/RepoController.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var app = angular.module("githubViewer");
+
+    var RepoController = function ($scope, github, $routeParams) {
+        var onRepoDetailsComplete = function (data) {
+            $scope.repo = data;
+        };
+
+        var onError = function (response) {
+            $scope.error = "Could not fetch the data.";
+        };
+
+        $scope.username = $routeParams.username;
+        $scope.reponame = $routeParams.reponame;
+        
+        github.getRepoDetails($scope.username, $scope.repoName)
+            .then(onRepoDetailsComplete, onError);
+    }
+
+    app.controller("RepoController", RepoController);
+}());
