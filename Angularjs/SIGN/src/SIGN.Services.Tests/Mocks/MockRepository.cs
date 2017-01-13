b97289485e0ab6ffd08930dbebb386a818b7cdae
gavinhickford <gjhickford@gmail.com>
2017-01-13 14:30:07 +0000

    Added Service unit tests
    Added Service unit tests

---
+++ Angularjs/SIGN/src/SIGN.Services.Tests/Mocks/MockRepository.cs
@@ -0,0 +1,137 @@
+﻿using SIGN.Domain.Classes;
+using SIGN.Domain.Enums;
+using SIGN.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGN.Services.Tests.Mocks
+{
+    public class MockRepository : ISIGNRepository
+    {
+        List<Guideline> _expectedGuidelines;
+
+        public MockRepository(List<Guideline> expectedGuidelines)
+        {
+            _expectedGuidelines = expectedGuidelines;
+        }
+
+        public MockRepository(int numberOfExpectedGuidelines)
+        {
+            _expectedGuidelines = new List<Guideline>();
+
+            for (int i = 1; i < numberOfExpectedGuidelines + 1; i++)
+            {
+                _expectedGuidelines.Add(
+                    new Guideline
+                    {
+                        Id = i,
+                        Name = $"TestGuideline{i}",
+                        Number = 100 + i,
+                        Author = "TestUser",
+                        DateCreated = new DateTime(2010, 1, 1),
+                        DateModified = new DateTime(2010, 1, 1),
+                        DatePublished = new DateTime(2005, 1, 1),
+                        Status = GuidelineStatus.GreaterThanSevenYears
+                    }
+                );
+            }
+        }
+
+        public void AddAssessment(int guidelineId, Assessment assessment)
+        {
+            return;
+        }
+
+        public void AddDecision(Decision decision)
+        {
+            return;
+        }
+
+        public void AddGuideline(Guideline guideline)
+        {
+            return;
+        }
+
+        public void AddStep(Step step)
+        {
+            return;
+        }
+
+        public void AddStepAction(StepAction stepAction)
+        {
+            return;
+        }
+
+        public StepAction GetAction(int stepId, bool choice)
+        {
+            return new StepAction
+            {
+                Id = 1,
+                DateCreated = new DateTime(2017, 1, 1),
+                DateModified = new DateTime(2017, 1, 1)
+            };
+        }
+
+        public Assessment GetAssessment(int id)
+        {
+            return new Assessment
+            {
+                Id = 1,
+                Name = "Test Assessment",
+                DateCreated = new DateTime(2017, 1, 1),
+                DateModified = new DateTime(2017, 1, 1),
+                Description = "Test Description",
+                Type = AssessmentType.Telephone
+            };
+        }
+
+        public Guideline GetGuideline(int id)
+        {
+            return _expectedGuidelines
+                .FirstOrDefault(g => g.Id == id);
+        }
+
+        public IEnumerable<Guideline> GetGuidelines()
+        {
+            return _expectedGuidelines;
+        }
+
+        public IEnumerable<Guideline> GetGuidelinesByAuthor(string AuthorName)
+        {
+            return new List<Guideline>
+            {
+                new Guideline {
+                    Id = 1,
+                    Author = AuthorName,
+                    DateCreated = new DateTime(2017, 1, 1),
+                    DateModified = new DateTime(2017, 1, 1),
+                    DatePublished = new DateTime(2017, 1, 1)
+                }
+            };
+        }
+
+        public Step GetStep(int id)
+        {
+            return new Step
+            {
+                Id = id,
+                Text ="Test Text",
+                DateCreated = new DateTime(2017, 1, 1),
+                DateModified = new DateTime(2017, 1, 1),
+                Type = StepType.Question
+            };
+        }
+
+        public int SaveChanges()
+        {
+            return 1;
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            return new Task<bool>(() => false);
+        }
+    }
+}
---
+++ Angularjs/SIGN/src/SIGN.Services.Tests/SIGNServiceTest.cs
@@ -0,0 +1,102 @@
+﻿using SIGN.Domain.Classes;
+using SIGN.Domain.Enums;
+using SIGN.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SIGN.Services.Tests
+{
+    
+    public class SIGNServiceTest
+    {
+        [Fact]
+        public void GetGuidlines_WhenRepositoryHasSingleGuideline_ReturnsSingleGuideline()
+        {
+            // Arrange
+            const int numberOfExpectedGuidelines = 1;
+
+            ISIGNRepository mockSignRepository = new Mocks.MockRepository(
+                numberOfExpectedGuidelines: numberOfExpectedGuidelines);
+            ISIGNService signService = new SIGNService(mockSignRepository);
+
+            // Act
+            IEnumerable<Guideline> actual = signService.GetGuidelines();
+
+            // Assert
+            Assert.Equal(numberOfExpectedGuidelines, actual.Count()); 
+        }
+
+        [Fact]
+        public void GetGuidlines_WhenRepositoryHasNoGuidelines_ReturnsEmptyList()
+        {
+            // Arrange
+            const int numberOfExpectedGuidelines = 0;
+
+            ISIGNRepository mockSignRepository = new Mocks.MockRepository(
+                numberOfExpectedGuidelines: numberOfExpectedGuidelines);
+            ISIGNService signService = new SIGNService(mockSignRepository);
+
+            // Act
+            IEnumerable<Guideline> actual = signService.GetGuidelines();
+
+            // Assert
+            Assert.Equal(numberOfExpectedGuidelines, actual.Count());
+        }
+
+        [Fact]
+        public void GetGuidlines_WhenRepositoryHasTenGuidelines_ReturnsTenGuidelines()
+        {
+            // Arrange
+            const int numberOfExpectedGuidelines = 10;
+
+            ISIGNRepository mockSignRepository = new Mocks.MockRepository(
+                numberOfExpectedGuidelines: numberOfExpectedGuidelines);
+            ISIGNService signService = new SIGNService(mockSignRepository);
+
+            // Act
+            IEnumerable<Guideline> actual = signService.GetGuidelines();
+
+            // Assert
+            Assert.Equal(numberOfExpectedGuidelines, actual.Count());
+        }
+
+        [Fact]
+        public void GetGuidline_WhenRepositoryIncludesId_ReturnsCorrectGuideline()
+        {
+            // Arrange
+            const int numberOfExpectedGuidelines = 10;
+            const int expectedIdentifier = 1;
+
+            ISIGNRepository mockSignRepository = new Mocks.MockRepository(
+                numberOfExpectedGuidelines: numberOfExpectedGuidelines);
+            ISIGNService signService = new SIGNService(mockSignRepository);
+
+            // Act
+            Guideline actual = signService.GetGuideline(expectedIdentifier);
+
+            // Assert
+            Assert.Equal(expectedIdentifier, actual.Id);
+        }
+
+        [Fact]
+        public void GetGuidline_WhenRepositoryDoesNotIncludeId_ReturnsNull()
+        {
+            // Arrange
+            const int numberOfExpectedGuidelines = 10;
+            const int expectedIdentifier = 11;
+
+            ISIGNRepository mockSignRepository = new Mocks.MockRepository(
+                numberOfExpectedGuidelines: numberOfExpectedGuidelines);
+            ISIGNService signService = new SIGNService(mockSignRepository);
+
+            // Act
+            Guideline actual = signService.GetGuideline(expectedIdentifier);
+
+            // Assert
+            Assert.Null(actual);
+        }
+
+    }
+}
