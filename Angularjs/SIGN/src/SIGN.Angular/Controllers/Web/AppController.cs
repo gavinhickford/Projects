ed6d9f5cbabe4182da06fe2d617688fa7ea37048
gavinhickford <gjhickford@gmail.com>
2017-01-21 22:00:06 +0000

    Removed unnecessary code form Angular project
    Removed unnecessary code form Angular project

--- Angularjs/SIGN/src/SIGN.Angular/Controllers/Web/AppController.cs
+++ Angularjs/SIGN/src/SIGN.Angular/Controllers/Web/AppController.cs
@@ -1,31 +1,17 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using SIGN.Angular.ViewModels;
-using SIGN.Domain.Classes;
-using SIGN.Domain.Interfaces;
 using System;
 
 namespace SIGN.Angular.Controllers.Web
 {
     public class AppController : Controller
     {
-        private IConfigurationRoot _configuration;
-        private ISIGNRepository _signRepository;
-        private IGuidelineService _guidelineService;
         private ILogger<AppController> _logger;
 
         public AppController(
-            ISIGNRepository signRepository, 
-            IGuidelineService guidelineService,
-            IConfigurationRoot configuration,
             ILogger<AppController> logger)
         {
-            _signRepository = signRepository;
-            _guidelineService = guidelineService;
-            _configuration = configuration;
             _logger = logger;
         }
         public IActionResult Index()
@@ -44,75 +30,28 @@ namespace SIGN.Angular.Controllers.Web
         [Authorize]
         public IActionResult Guidelines()
         {
-            //GuidelinesViewModel model = new GuidelinesViewModel();
-            //model.Guidelines = _signRepository.GetGuidelines();
-            return View();
-        }
-
-        [Authorize]
-        public IActionResult MyGuidelines()
-        {
-            GuidelinesViewModel model = new GuidelinesViewModel();
-            model.Guidelines = _guidelineService.GetMyGuidelines(User.Identity.Name);
-            return View(model);
-        }
-
-        public IActionResult Guideline(int id)
-        {
-            Guideline guideline = _guidelineService.GetGuideline(id);
-            GuidelineViewModel model = Mapper.Map<GuidelineViewModel>(guideline);
-
-            return View(model);
-        }
-
-        public IActionResult Assessment(int id)
-        {
-            Assessment assessment = _signRepository.GetAssessment(id);
-            AssessmentViewModel model = Mapper.Map<AssessmentViewModel>(assessment);
-
-            return View(model);
-        }
-
-        public IActionResult Step(int id)
-        {
-            Step step = _signRepository.GetStep(id);
-            StepViewModel model = Mapper.Map<StepViewModel>(step);
-
-            int? yesStepId = GetNextStepId(step.Id, true);
-            int? noStepId = GetNextStepId(step.Id, false);
-
-            if (yesStepId.HasValue)
+            try
             {
-                model.YesStepId = yesStepId.Value;
-            };
-
-            if (noStepId.HasValue)
-            {
-                model.NoStepId = noStepId.Value;
-            };
-
-            return View(model);
-        }
-
-        private int? GetNextStepId(int stepId, bool condition)
-        {
-            StepAction action = _signRepository.GetAction(stepId, condition);
-
-            if (action != null)
-            {
-                Step step = action.NextStep;
-                if (step != null)
-                {
-                    return step.Id;
-                }
+                return View();
             }
-            
-            return null;
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting the Guidelines page: {ex.Message}");
+                return Redirect("/error");
+            }
         }
 
         public IActionResult About()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting the About page: {ex.Message}");
+                return Redirect("/error");
+            }
         }
     }
 }
