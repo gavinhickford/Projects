781f5b93aba041f50dbc047a59ed43b61b52e651
gavinhickford <gjhickford@gmail.com>
2017-01-21 22:15:48 +0000

    Removed redundant code
    Removed redundant code

--- Angularjs/SIGN/src/SIGN.MVC/Controllers/Web/AppController.cs
+++ Angularjs/SIGN/src/SIGN.MVC/Controllers/Web/AppController.cs
@@ -1,28 +1,18 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using SIGN.Domain.Classes;
 using SIGN.Domain.Interfaces;
-using SIGN.MVC.ViewModels;
 using System;
 
 namespace SIGN.MVC.Controllers.Web
 {
     public class AppController : Controller
     {
-        private IConfigurationRoot _configuration;
-        private IGuidelineService _guidelineService;
         private ILogger<AppController> _logger;
 
         public AppController(
-            IGuidelineService guidelineService, 
-            IConfigurationRoot configuration,
             ILogger<AppController> logger)
         {
-            _guidelineService = guidelineService;
-            _configuration = configuration;
             _logger = logger;
         }
         public IActionResult Index()
@@ -40,7 +30,15 @@ namespace SIGN.MVC.Controllers.Web
         
         public IActionResult About()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting the about page: {ex.Message}");
+                return Redirect("/error");
+            }
         }
     }
 }
