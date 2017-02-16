8b7c44782252ddb8474d8e8c5ef990e01d0b6393
gavinhickford <gjhickford@gmail.com>
2017-02-16 18:59:38 +0000

    Added dom interaction examples
    Added dom interaction examples

---
+++ JQuery/JQuery/scripts/interact.js
@@ -0,0 +1,31 @@
+﻿// interact.js
+
+function divIterate() {
+    $(".div-select").each(function(index, element) {
+        alert(index + " = " + $(element).text());
+    });
+}
+
+function divUpdateDirectly() {
+    $(".div-select").each(function (index) {
+        this.title = "Updated directly. Index = " + index;
+    });
+}
+
+function divUpdateWithAttributes() {
+    $(".div-select").each(function (index) {
+        $(this).attr("title", "Updated with attr. Index = " + index);
+    });
+}
+
+function divUpdateMultipleAttributes() {
+    $(".div-select").each(function (index) {
+        var text = "Updated multiple attributes. Index = " + index;
+        $(this).attr(
+            {
+                title: text,
+                style: "border: 2px solid black;"
+            }
+        );
+    });
+}
