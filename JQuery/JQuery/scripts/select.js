a6088d0dc094fe2f2118f2ffff3422ce058d9013
gavinhickford <gjhickford@gmail.com>
2017-02-16 15:05:16 +0000

    Added sample JQuery project
    Added sample JQuery project

---
+++ JQuery/JQuery/scripts/select.js
@@ -0,0 +1,54 @@
+﻿// select.js
+
+function selectById(e) {
+    var divById = $("#SpecificDiv");
+    toggleClass(e.checked, divById);
+}
+
+function selectByTag(e) {
+    var divsByTag = $("div");
+    toggleClass(e.checked, divsByTag);
+}
+
+function selectByClass(e) {
+    var divByClass = $(".div-class");
+    toggleClass(e.checked, divByClass);
+}
+
+function selectByAttribute(e) {
+    var divsByArribute = $("div[title='This is the title text']");
+    toggleClass(e.checked, divsByArribute);
+}
+
+function selectByContains(e) {
+    var divContains = $("div:contains('umbrella')");
+    toggleClass(e.checked, divContains);
+}
+
+function selectByStartsWith(e) {
+    var divStartsWith = $("div[title^='Starts']");
+    toggleClass(e.checked, divStartsWith);
+}
+
+function selectByEndsWith(e) {
+    var divEndsWith = $("div[title$='Starts']");
+    toggleClass(e.checked, divEndsWith);
+}
+
+function selectByTitleContains(e) {
+    var divContains = $("div[title*='Starts']");
+    toggleClass(e.checked, divContains);
+}
+
+function selectInputs(e) {
+    var inputs = $(":input");
+    toggleClass(e.checked, inputs);
+}
+
+function toggleClass(checked, element) {
+    if (checked) {
+        element.addClass("selected");
+    } else {
+        element.removeClass("selected");
+    }
+}
