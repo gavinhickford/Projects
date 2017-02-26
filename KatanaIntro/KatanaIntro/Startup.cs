d68bfa54438b82d4dcbe9bac1083d9e9072cc119
gavinhickford <gjhickford@gmail.com>
2017-02-26 09:41:45 +0000

    Added simple environment logging
    Added simple environment logging

--- KatanaIntro/KatanaIntro/Startup.cs
+++ KatanaIntro/KatanaIntro/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Owin;
+using System;
 
 namespace KatanaIntro
 {
@@ -6,6 +7,25 @@ namespace KatanaIntro
     {
         public void Configuration(IAppBuilder app)
         {
+            app.Use(async (environment, next) =>
+            {
+                foreach (var pair in environment.Environment)
+                {
+                    Console.WriteLine($"{pair.Key}:{pair.Value}");
+                }
+
+                await next();
+            });
+
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine($"Requesting: {environment.Request.Path}");
+
+                await next();
+
+                Console.WriteLine($"Response: {environment.Response.StatusCode}");
+            });
+
             app.UseHelloWorld();
         }
     }
